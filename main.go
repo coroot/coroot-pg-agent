1efb931d793963d54ad3accb8a26dc2c77cddaef
Nikolay Sivko <n.sivko@gmail.com>
2022-07-26 14:58:24 +0300

    added the ability to set static labels to all metrics (closes #4)

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"gopkg.in/alecthomas/kingpin.v2"
+	"k8s.io/klog/v2"
 	"net/http"
 	_ "net/http/pprof"
 )
@@ -15,7 +16,8 @@ var version = "unknown"
 func main() {
 	dsn := kingpin.Arg("dsn", `Data source name (env: DSN) - "postgresql://<user>:<password>@<host>:5432/postgres?connect_timeout=1&statement_timeout=30000".`).Envar("DSN").Required().String()
 	listen := kingpin.Flag("listen", `Listen address (env: LISTEN) - "<ip>:<port>" or ":<port>".`).Envar("LISTEN").Default("0.0.0.0:80").String()
-	scrapeInterval := kingpin.Flag("scrape-interval", `How often to snapshot system views`).Envar("PG_SCRAPE_INTERVAL").Default("15s").Duration()
+	scrapeInterval := kingpin.Flag("scrape-interval", `How often to snapshot system views (env: PG_SCRAPE_INTERVAL)`).Envar("PG_SCRAPE_INTERVAL").Default("15s").Duration()
+	staticLabels := kingpin.Flag("label", `A static label:value pair to be added to all metrics (env: STATIC_LABELS)`).Envar("STATIC_LABELS").StringMap()
 
 	kingpin.HelpFlag.Short('h').Hidden()
 	kingpin.Version(version)
@@ -29,11 +31,14 @@ func main() {
 		return
 	}
 
-	reg := prometheus.NewRegistry()
-	reg.MustRegister(info("pg_agent_info", version))
-	reg.MustRegister(c)
+	registry := prometheus.NewRegistry()
 
-	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+	klog.Infoln("static labels:", *staticLabels)
+	registerer := prometheus.WrapRegistererWith(*staticLabels, registry)
+	registerer.MustRegister(info("pg_agent_info", version))
+	registerer.MustRegister(c)
+
+	http.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
 	log.Info("listening on:", *listen)
 	log.Error(http.ListenAndServe(*listen, nil))
 }
